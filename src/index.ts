43e61a808142cbd3991440511db1a8001f3a42f0
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-05-07 00:24:33 -0500

    05.07.25

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: = true;
+
+let level;
+level = 1;
+level = 'a';
+
+function render(document){
+    console.log(document);
+}
+
